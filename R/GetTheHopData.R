159fc0db4b812a1898e06bb90c0a94a07f60654e
dempseydata <dempseydata@gmail.com>
2017-02-03 21:28:12 -0800

    za

---
+++ R/GetTheHopData.R
@@ -0,0 +1,19 @@
+# use rvest to get HOPS data, and wranlge to a dataframe for saving
+library(rvest)
+
+# web reference: http://blog.corynissen.com/2015/01/using-rvest-to-scrape-html-table.html
+# web source: http://www.schiesshouse.com/hop_properties_chart.htm
+# note, use "inspect" in Chrome to find the right tabel XPath
+# /html/body/table
+
+# the web server rejects the request for this URL
+# url <- "http://www.schiesshouse.com/hop_properties_chart.htm"
+
+# this one does not
+url <- "https://ychhops.com/varieties"
+
+hops_raw <- read_html(url) %>%
+    html_nodes(xpath='//*[@id="adminForm"]/table') %>%
+    html_table()
+
+
